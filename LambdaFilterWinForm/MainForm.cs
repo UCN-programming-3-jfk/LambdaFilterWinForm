8095adf8de245d838b80380d3d14ee39c459a668
Jakob Farian Krarup <kr@rup.dk>
2021-10-12 21:48:31 +0200

    Added more flowers

--- LambdaFilterWinForm/MainForm.cs
+++ LambdaFilterWinForm/MainForm.cs
@@ -41,6 +41,8 @@ namespace LambdaFilterWinForm
             _plants.Add(new Plant() { Name = "Bonsai Weed", Color = Color.Blue, HeightInCm = 80, ToleratesShade = true });
             _plants.Add(new Plant() { Name = "Mule stalk", Color = Color.White, HeightInCm = 20, ToleratesShade = true });
             _plants.Add(new Plant() { Name = "Little starstalk", Color = Color.Yellow, HeightInCm = 10, ToleratesShade = true });
+            _plants.Add(new Plant() { Name = "Tall starstalk", Color = Color.Yellow, HeightInCm = 120, ToleratesShade = true });
+            _plants.Add(new Plant() { Name = "Tall Bloodstalk", Color = Color.Yellow, HeightInCm = 120, ToleratesShade = false });
             _plants.Add(new Plant() { Name = "Common grass", Color = Color.Green, HeightInCm = 12, ToleratesShade = true });
             _plants.Add(new Plant() { Name = "Orchard climber", Color = Color.Purple, HeightInCm = 180, ToleratesShade = false });
         }
